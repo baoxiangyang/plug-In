8965efbaaaf97bd27f068784626e8387cc12e7a6
baoxiaoying <791736879@qq.com>
2016-05-25 09:49:44 +0800

    生成excel

---
+++ excel.js
@@ -0,0 +1,20 @@
+var excelPort = require('excel-export');
+var fs = require('fs')
+var conf = {};
+conf.cols = [
+   {caption:'名称', type:'string', width:20},
+   {caption:'简介', type:'string', width:40},
+   {caption:'报酬', type:'string', width:20}
+];
+conf.rows = [
+	['aaa','bbb','ccc'],['sdf','sdf','33fsf3']
+]
+var result = excelPort.execute(conf);
+var filePath = './test.xlsx';
+fs.writeFile(filePath, result, 'binary',function(err){
+    if(err){
+        console.log(err);
+    }else{
+    	console.log('success');
+    }
+});
--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@ slipe 移动端滑动插件(原生js)。用法和参数请看slipe.js、slipe.ht
 dropUpload pc端文件拖拽上传demo<br/>
 rediusLoad.html 圆形加载插件
 getMedia.html 获取摄像头，麦克风
+excel.js 数据生成excel。
